19900ede20280e0986f87730b87e693ede78bf30
Rohitdas-01 <rohit.41.das@gmail.com>
2026-02-10 12:33:31 +0530

    Add New Function

--- practice.py
+++ practice.py
@@ -6,3 +6,12 @@ num2 = int(input("Enter numbers separated by spaces: "))
 
 add = addition(num1, num2)
 print("Addition of numbers:",add)
+
+def substraction(a,b):
+    return a-b
+
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+
+sub = substraction(number1,number2)
+print(f' Substraction of {number1} and {number2} =', sub)
