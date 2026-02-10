f7b7da8c0a67dd7619e0fdf765247d5cb70f4ffc
Rohitdas-01 <rohit.41.das@gmail.com>
2026-02-10 12:40:07 +0530

    Add new function multiplication

--- practice.py
+++ practice.py
@@ -6,3 +6,12 @@ num2 = int(input("Enter numbers separated by spaces: "))
 
 add = addition(num1, num2)
 print("Addition of numbers:",add)
+
+def multiplication(a,b):
+    return a*b
+
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+
+sub = multiplication(number1,number2)
+print(f' multiplication of {number1} and {number2} =', sub)
